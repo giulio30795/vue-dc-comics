0ce7b0120b8450090e46693e9743dfe7e4b905c5
giulio30795 <giulioincarbone95@gmail.com>
2021-11-23 16:43:21 +0100

    Header

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
 <template>
   <div id="app">
     <Header />
+    <main>
+      
+    </main>
   </div>
 </template>
 
 <script>
 
 import Header from '@/components/Header.vue'
+  
+
 export default {
   name: 'App',
   components: {
@@ -16,12 +21,5 @@ export default {
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
