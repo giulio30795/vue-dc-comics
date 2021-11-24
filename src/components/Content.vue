17200c4ae4e3c071488c7067fe24c6766fe66966
giulio30795 <giulioincarbone95@gmail.com>
2021-11-24 17:17:02 +0100

    check

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -1,26 +1,55 @@
 <template>
     <section>
-        <div class="container content">******Content Goes Here******</div>
+        <div class="container">
+            <ul class="thumb-list flex">
+                <li v-for="(element, index) in comics" :key="`list-${index}`">
+                    <div class="img-container">
+                        <img :src="element.thumb" :alt="element.series">
+                    </div>
+                    <h4>{{element.series}}</h4>
+                </li>
+            </ul>
+        </div>
     </section>
 </template>
 
 <script>
+import comicsList from '@/data/dc-comics.js'
 export default {
-    name: 'Content'
+    name: 'Content',
 
+    data(){
+        return {
+            comics: comicsList
+        }
+    }
 }
 </script>
 
 <style scped lang="scss">
 @import '@/styles/general';
 
+section
+.thumb-list{
+    padding-top: 2.5rem;
+    color:#fff;
+    flex-wrap: wrap;
+    li{
+        width: calc(100% / 6);
+        padding: .5rem;
+        .img-container{
+            height: 150px;
+            
+            img{
+                object-fit: cover;
+                height: 100%;
+                width: 100%;
+                object-position: top;
+            }
+        }
+    }
+}
 section {
     background: #000;
-}
-.content{
-    font-size: 3rem;
-    color: #fff;
-    padding: 1rem;
-    text-align: center;
 }
 </style>
